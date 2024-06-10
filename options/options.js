3c7f552ea9473794b5516c75c46a62c223671d4e
Crowdbotics <team@crowdbotics.com>
2024-06-10 12:40:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "book-creation-gener-48335",
+  url: "https://book-creation-gener-48335.botics.co",
+  api: "https://book-creation-gener-48335.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
